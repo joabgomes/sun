f095714f74f201c5b5f72f9c965e9e488b2992a6
codeBeachD <douglasbtst30@gmail.com>
2017-05-08 20:45:32 -0300

    porta 3000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require('./config/express')();
 require('./config/database')('mongodb://localhost/sundb');
 
-app.listen(8080,function(){
+app.listen(3000,function(){
     console.log('O servidor foi iniciado!');
 });
