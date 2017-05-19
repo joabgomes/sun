f8b135fd3776783523f7f0c305d69f8e6ad902e5
codeBeachD <douglasbtst30@gmail.com>
2017-05-19 16:16:48 -0300

    Alterações do módulo vendas 0.1

--- app/core/vendas/vendas.api.js
+++ app/core/vendas/vendas.api.js
@@ -3,15 +3,14 @@ const VendasService = require('./vendas.service');
 const api = {
   adiciona: adicionarVendaAPI
 };
-
 module.exports = api;
 /**
  * POST /vendas
  */
 function adicionarVendaAPI(req,res){
   VendasService.adicionarVenda(req.body)
-    .then(function(vendas){
-       return res.json(vendas);
+   .then(function(vendas){
+     return res.json(vendas);
 },function(error){
     return res.status(500).json(error);
 });
