31cd66b40c7f644fba72c411f9bc2e4cd462e589
Álvaro das Mercês <alvaro.dasmerces@gmail.com>
2017-05-21 21:50:01 -0400

    Correção no require do modulo de log

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var app = require('./config/express')();
-var logger = require('app/util/logger');
+var logger = require('./app/util/logger');
 require('./config/database')('mongodb://localhost/sundb');
 
 app.listen(3000, '0.0.0.0', function () {
